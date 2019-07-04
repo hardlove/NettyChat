e577f92bab8d170fafa584eb3b105fa94b8356ad
CL <413162035@qq.com>
2019-07-04 12:24:38 +0800

    fix

--- app/src/main/java/com/freddy/chat/utils/encry/AESUtil.java
+++ app/src/main/java/com/freddy/chat/utils/encry/AESUtil.java
@@ -1,6 +1,7 @@
 package com.freddy.chat.utils.encry;
 
 
+import android.os.Handler;
 
 import org.apache.commons.codec.binary.Base64;
 
@@ -19,7 +20,7 @@ import javax.crypto.spec.SecretKeySpec;
 
 public class AESUtil {
     //生成AES秘钥，然后Base64编码
-    public static String genKeyAES() throws Exception{
+    public static String genKeyAES() throws Exception {
         KeyGenerator keyGen = KeyGenerator.getInstance("AES");
         keyGen.init(128);
         SecretKey key = keyGen.generateKey();
@@ -28,38 +29,45 @@ public class AESUtil {
     }
 
     //将Base64编码后的AES秘钥转换成SecretKey对象
-    public static SecretKey loadKeyAES(String base64Key) throws Exception{
+    public static SecretKey loadKeyAES(String base64Key) throws Exception {
         byte[] bytes = base642Byte(base64Key);
         SecretKeySpec key = new SecretKeySpec(bytes, "AES");
         return key;
     }
 
     //加密
-    public static byte[] encryptAES(byte[] source, SecretKey key) throws Exception{
+    public static byte[] encryptAES(byte[] source, SecretKey key) throws Exception {
         Cipher cipher = Cipher.getInstance("AES");
         cipher.init(Cipher.ENCRYPT_MODE, key);
         return cipher.doFinal(source);
     }
 
 
-
     //解密
-    public static byte[] decryptAES(byte[] source, SecretKey key) throws Exception{
+    public static byte[] decryptAES(byte[] source, SecretKey key) throws Exception {
         Cipher cipher = Cipher.getInstance("AES");
         cipher.init(Cipher.DECRYPT_MODE, key);
         return cipher.doFinal(source);
     }
 
     //字节数组转Base64编码
-    public static String byte2Base64(byte[] bytes){
-//        return android.util.Base64.encodeToString(bytes, android.util.Base64.DEFAULT);
-        return Base64.encodeBase64String(bytes);
+    public static String byte2Base64(byte[] bytes) {
+        if (HttpEncryptUtil.useAndroidBase64) {
+            return android.util.Base64.encodeToString(bytes, android.util.Base64.DEFAULT);
+        } else {
+            return Base64.encodeBase64String(bytes);
+        }
+
     }
 
     //Base64编码转字节数组
     public static byte[] base642Byte(String base64Key) throws IOException {
-//        return android.util.Base64.decode(base64Key, android.util.Base64.DEFAULT);
-        return Base64.decodeBase64(base64Key);
+        if (HttpEncryptUtil.useAndroidBase64) {
+            return android.util.Base64.decode(base64Key, android.util.Base64.DEFAULT);
+        } else {
+            return Base64.decodeBase64(base64Key);
+        }
+
     }
 
 }
--- app/src/main/java/com/freddy/chat/utils/encry/HttpEncryptUtil.java
+++ app/src/main/java/com/freddy/chat/utils/encry/HttpEncryptUtil.java
@@ -11,13 +11,19 @@ import org.apache.commons.codec.binary.Base64;
 
 public class HttpEncryptUtil {
 
+   static boolean useAndroidBase64 = false;
+
     /**
      * 获取AES加密后的Prk
      */
     public static String getEncrptyPrk() throws Exception {
         //加密app生成的AES秘钥
-//      return   android.util.Base64.encodeToString(AESUtil.encryptAES(android.util.Base64.decode(KeyUtil.APP_AES_KEY, android.util.Base64.DEFAULT), AESUtil.loadKeyAES(KeyUtil.SERVER_AES_KEY)), android.util.Base64.DEFAULT);
-        return Base64.encodeBase64String(AESUtil.encryptAES(Base64.decodeBase64(KeyUtil.APP_AES_KEY), AESUtil.loadKeyAES(KeyUtil.SERVER_AES_KEY)));
+        if (useAndroidBase64) {
+            return android.util.Base64.encodeToString(AESUtil.encryptAES(android.util.Base64.decode(KeyUtil.APP_AES_KEY, android.util.Base64.DEFAULT), AESUtil.loadKeyAES(KeyUtil.SERVER_AES_KEY)), android.util.Base64.DEFAULT);
+        } else {
+            return Base64.encodeBase64String(AESUtil.encryptAES(Base64.decodeBase64(KeyUtil.APP_AES_KEY), AESUtil.loadKeyAES(KeyUtil.SERVER_AES_KEY)));
+
+        }
 
     }
 
@@ -29,8 +35,13 @@ public class HttpEncryptUtil {
      * @throws Exception
      */
     public static String encrptyData(String data) throws Exception {
-        return Base64.encodeBase64String(AESUtil.encryptAES(data.getBytes(), AESUtil.loadKeyAES(KeyUtil.APP_AES_KEY)));
-//        return android.util.Base64.encodeToString(AESUtil.encryptAES(data.getBytes("UTF-8"), AESUtil.loadKeyAES(KeyUtil.APP_AES_KEY)), android.util.Base64.DEFAULT);
+        if (useAndroidBase64) {
+            return android.util.Base64.encodeToString(AESUtil.encryptAES(data.getBytes("UTF-8"), AESUtil.loadKeyAES(KeyUtil.APP_AES_KEY)), android.util.Base64.DEFAULT);
+        } else {
+            return Base64.encodeBase64String(AESUtil.encryptAES(data.getBytes(), AESUtil.loadKeyAES(KeyUtil.APP_AES_KEY)));
+
+        }
+
     }
 
     /**
@@ -42,10 +53,15 @@ public class HttpEncryptUtil {
      */
     public static String getDecrptyPrk(String prk) throws Exception {
         //解密app生成的AES秘钥
-        byte[] decryptAES = AESUtil.decryptAES(Base64.decodeBase64(prk), AESUtil.loadKeyAES(KeyUtil.SERVER_AES_KEY));
-        return Base64.encodeBase64String(decryptAES);
+        if (useAndroidBase64) {
+            return android.util.Base64.encodeToString(AESUtil.decryptAES(android.util.Base64.decode(prk, android.util.Base64.DEFAULT), AESUtil.loadKeyAES(KeyUtil.SERVER_AES_KEY)), android.util.Base64.DEFAULT);
+        } else {
+            byte[] decryptAES = AESUtil.decryptAES(Base64.decodeBase64(prk), AESUtil.loadKeyAES(KeyUtil.SERVER_AES_KEY));
+            return Base64.encodeBase64String(decryptAES);
+        }
 
-//        return android.util.Base64.encodeToString(AESUtil.decryptAES(android.util.Base64.decode(prk, android.util.Base64.DEFAULT), AESUtil.loadKeyAES(KeyUtil.SERVER_AES_KEY)), android.util.Base64.DEFAULT);
+
+
 
     }
 
@@ -59,10 +75,16 @@ public class HttpEncryptUtil {
      */
     public static String decrptyData(String prk, String data) throws Exception {
         //解密data
-        byte[] decrpytBytes = AESUtil.decryptAES(Base64.decodeBase64(data), AESUtil.loadKeyAES(prk));
-        return new String(decrpytBytes);
-//        byte[] decrpytBytes = AESUtil.decryptAES(android.util.Base64.decode(data, android.util.Base64.DEFAULT), AESUtil.loadKeyAES(prk));
-//        return new String(decrpytBytes, "UTF-8");
+        if (useAndroidBase64) {
+            byte[] decrpytBytes = AESUtil.decryptAES(android.util.Base64.decode(data, android.util.Base64.DEFAULT), AESUtil.loadKeyAES(prk));
+            return new String(decrpytBytes, "UTF-8");
+        } else {
+            byte[] decrpytBytes = AESUtil.decryptAES(Base64.decodeBase64(data), AESUtil.loadKeyAES(prk));
+            return new String(decrpytBytes);
+        }
+
+
+
     }
 
 
